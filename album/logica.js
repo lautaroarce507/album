16c51e204f90e3d769fcad55bbc45f9972cd5dd3
lautaro <lautaroarce507@gmail.com>
2026-05-08 20:37:27 -0300

    cambio

--- album/logica.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-    card.addEventListener("click", () => {
-        card.classList.remove("locked");
-        card.classList.add("unlocked");
-    });
-});
